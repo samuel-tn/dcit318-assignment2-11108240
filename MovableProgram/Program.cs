83122503a5f1fe686cc8deaec9353a9f76d29c10
tandoh22 <tandohsamuel4u@gmail.com>
2025-08-08 12:10:04 +0000

    Interfaces

---
+++ MovableProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MovableProgram
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            IMovable myCar = new Car();
+            IMovable myBicycle = new Bicycle();
+
+            myCar.Move();
+            myBicycle.Move();
+        }
+    }
+}
+
