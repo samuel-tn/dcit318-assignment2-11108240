bfd738d9f910f5573430a2e3226ff90e79dcc52b
tandoh22 <tandohsamuel4u@gmail.com>
2025-08-08 11:38:55 +0000

    Inheritance and Method Overriding

---
+++ AnimalSoundProgram/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AnimalSoundProgram
+{
+    
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            animal.MakeSound(); 
+            dog.MakeSound();           
+            cat.MakeSound();          
+        }
+    }
+}
+
